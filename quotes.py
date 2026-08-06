48c5c9e1a13d0d79b65c0ed327c6e899bd938e94
animatrix49 <anirudhkottayil@gmail.com>
2026-08-06 17:06:15 +1000

    feat: add save last quote functionality

--- quotes.py
+++ quotes.py
@@ -1,35 +1,38 @@
 import requests
-import os
-import argparse
-from pathlib import Path
+import json
+from init_check import init_check
 
-QUOTES_DIR = Path("~/.local/share/quotes/").expanduser()
+def last_quote_write(QUOTES_DIR):
+    with open((QUOTES_DIR / "last_shown.jsonl"), "r+") as file:
+        line = file.readline()
+        if line == '':
+            print("No saved last quote found")
+            return
 
+        content = json.loads(line)
+        try:
+            with open((QUOTES_DIR / "storage.jsonl"), "a") as f:
+                f.write(json.dumps(content) + '\n')
+        except OSError as e:
+            print(f"Write failed: {e}")
 
-def init_files() -> None:
-    (QUOTES_DIR / "last_shown.jsonl").touch(exist_ok=True)
-    (QUOTES_DIR / "cache.jsonl").touch(exist_ok=True)
-    (QUOTES_DIR / "storage.jsonl").touch(exist_ok=True)
-    (QUOTES_DIR / ".initialized").touch(exist_ok=True)
+        file.truncate(0)
+    print("Quote Saved")
 
-
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="This tool is to get quotes")
-    parser.add_argument("--save", action="store_true", help="To save last quote")
-    parser.add_argument("--add", action="store_true", help="To add your own quote")
-    return parser.parse_args()
+def user_add_quote(QUOTES_DIR):
 
 
 def main() -> None:
     url = "https://zenquotes.io/api/quotes"
+    args, QUOTES_DIR = init_check() # File checks and input flags
 
-    if not QUOTES_DIR.exists():
-        QUOTES_DIR.mkdir(parents=True, exist_ok=True)
-        init_files()
-    if not (QUOTES_DIR / ".initialized").exists():
-        init_files()
+    if args.save:
+        last_quote_write(QUOTES_DIR)
+    if args.add:
+        user_add_quote(QUOTES_DIR)
+    if args.save or args.add:
+        return
 
-    args = parse_args()
 
 
 if __name__ == "__main__":
