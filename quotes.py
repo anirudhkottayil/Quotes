3bcd1550b552ea4efeb9dc2a195209d578df8dbf
animatrix49 <anirudhkottayil@gmail.com>
2026-08-06 22:49:20 +1000

    refactor: modularize get request and cache access in print_quote.py

--- quotes.py
+++ quotes.py
@@ -1,55 +1,7 @@
-import requests
-import json
-import os
 from init_check import init_check
 from args_check import args_run
+from print_quote import print_quote
 
-def get_quotes(QUOTES_DIR, url) -> int:
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(f"Get request failed: {e}")
-        return 1
-
-    quotes = r.json()
-    try:
-        with open((QUOTES_DIR / "cache.jsonl"), "w") as file:
-            for line in quotes:
-                file.write(json.dumps(line) + '\n')
-    except OSError as e:
-        print(f"Cache write failed: {e}")
-        return 1
-    else:
-        return 0
-
-
-def check_cache(QUOTES_DIR) -> int:
-    with open(QUOTES_DIR / "cache.jsonl"), "r+b") as file:
-        file.seek(0, os.SEEK_END)
-        pos = file.tell()
-
-        if pos == 0:
-            return 1
-
-        file.seek(pos - 1)
-        if file.read(1) == b'\n':
-            pos -= 1
-
-        while (pos > 0):
-            file.seek(pos - 1)
-            if file.read(1) == '\n':
-                break
-            pos -= 1
-
-        file.seek(pos)
-        line = file.readline()
-        file.seek(pos)
-        file.truncate()
-
-        content = json.loads(line)
-        print(f"{content["q"]} - {content["a"]}")
-        return 0
 
 def main() -> None:
     url = "https://zenquotes.io/api/quotes"
@@ -58,14 +10,8 @@ def main() -> None:
 
     if args_run(args, QUOTES_DIR) == 1: # Checks and completes input flags functionality
         return
-    if check_cache(QUOTES_DIR) == 1:
-        if get_quotes(QUOTES_DIR, url) == 1:
-            print("Failed")
-            return 1
-        if check_cache(QUOTES_DIR) == 1:
-            print("Failed")
-            return 1
-    return 0
+    if print_quote(QUOTES_DIR, url) == 1:
+        return
 
 if __name__ == "__main__":
     main()
